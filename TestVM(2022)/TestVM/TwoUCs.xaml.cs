4956f10cdaf076324cbc01ece7ef5209e0098010
Хасп Элд <EldHasp@Gmail.com>
2022-02-07 19:46:01 +0300

    Пример с переключением UC'ов кнопкой.

---
+++ TestVM(2022)/TestVM/TwoUCs.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TestVM
+{
+    /// <summary>
+    /// Логика взаимодействия для TwoUCs.xaml
+    /// </summary>
+    public partial class TwoUCs : Window
+    {
+        public TwoUCs()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSwap(object sender, RoutedEventArgs e)
+        {
+            UserControl one = (UserControl)FindResource(nameof(one));
+            UserControl two = (UserControl)FindResource(nameof(two));
+            if (presenter.Content != one)
+            {
+                presenter.Content = one;
+            }
+            else
+            {
+                presenter.Content = two;
+            }
+        }
+    }
+}
